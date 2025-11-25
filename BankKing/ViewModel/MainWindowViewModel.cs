8e9d51802e7acab3ff3748d781847c01caef9043
Léo Puyastier <puyastier.leo@gmail.com>
2025-11-25 17:29:09 +0100

    Ajout de la méthode d'aide des Commandes (boutons)

---
+++ BankKing/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using BankKing.Data.Account;
+using BankKing.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace BankKing.ViewModel;
+
+public class MainWindowViewModel(IAccountService accountService) : INotifyPropertyChanged
+{
+    private ObservableCollection<Account>? Accounts
+    {
+        get;
+        set;
+    }
+
+    private IAccountService _accountService = accountService;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged(string name)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
