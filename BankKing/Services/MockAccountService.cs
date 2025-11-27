ce28e37fdaf44bae88681a3918138fba8fce53f9
Léo Puyastier <puyastier.leo@gmail.com>
2025-11-27 16:10:03 +0100

    Mock des données

--- BankKing/Services/MockAccountService.cs
+++ BankKing/Services/MockAccountService.cs
@@ -1,4 +1,5 @@
 ﻿using BankKing.Data.Account;
+using BankKing.Data.Entry;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -24,9 +25,56 @@ public class MockAccountService : IAccountService
 
     public ObservableCollection<Account> GetAccounts()
     {
+        EntryCategory cat1 = new()
+        {
+            Name = "Nourriture",
+            Type = EntryType.Expense
+        };
+
+        List<AccountEntry> entries = [];
+        entries.Add(new AccountEntry()
+        {
+            Category = cat1,
+            Amount = -50.75,
+            Date = DateTime.Now.AddDays(-2)
+        });
+        entries.Add(new AccountEntry()
+        {
+            Category = cat1,
+            Amount = -23.40,
+            Date = DateTime.Now.AddDays(-7)
+        });
+
+        EntryCategory cat2 = new()
+        {
+            Name = "Salaire",
+            Type = EntryType.Income
+        };
+
+        entries.Add(new AccountEntry()
+        {
+            Category = cat2,
+            Amount = 2500.00,
+            Date = DateTime.Now.AddDays(-2)
+        });
+
+        EntryCategory cat3 = new()
+        {
+            Name = "Impôts",
+            Type = EntryType.Expense
+        };
+
+        entries.Add(new AccountEntry()
+        {
+            Category = cat3,
+            Amount = -150.00,
+            Date = DateTime.Now.AddDays(-1)
+        });
+
+
         // In a real application, this method would retrieve data from a database
         return new ObservableCollection<Account>([
-            new() { Name = "Compte1", Balance = 1000 },
+            new() { Name = "Compte1", Balance = 1000, Entries = entries },
             new() { Name = "Compte2", Balance = 3272.12 },
             new() { Name = "Compte3", Balance = 56.75 },
             new() { Name = "Compte4", Balance = 56.75 },
