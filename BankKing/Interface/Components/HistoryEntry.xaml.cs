97a9b3fbeff718b6eae01db45a0116545d5a46ed
Léo Puyastier <puyastier.leo@gmail.com>
2025-12-02 18:50:44 +0100

    Attribution du style

---
+++ BankKing/Interface/Components/HistoryEntry.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BankKing.Interface.Components
+{
+    /// <summary>
+    /// Logique d'interaction pour HistoryItem.xaml
+    /// </summary>
+    public partial class HistoryEntry : UserControl
+    {
+        public HistoryEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
