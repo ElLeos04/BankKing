3523ac5f3889989c2b86d6ebb5e3a24c9f3c7396
Léo Puyastier <puyastier.leo@gmail.com>
2025-12-05 17:09:38 +0100

    Suppression des logs

--- BankKing/Data/XMLAccountSaver.cs
+++ BankKing/Data/XMLAccountSaver.cs
@@ -1,5 +1,4 @@
 ﻿using BankKing.Data.Account;
-using System.Diagnostics;
 using System.IO;
 using System.Xml.Serialization;
 
@@ -11,24 +10,18 @@ namespace BankKing.Data
 
         public void SaveAccount(BankAccount account)
         {
-            Trace.WriteLine("Début de l'écriture");
-
             XmlSerializer serializer = new XmlSerializer(typeof(BankAccount));
             string filePath = FOLDER_PATH + account.Name + ".xml";
 
             if (!Directory.Exists(FOLDER_PATH))
             {
-                Trace.WriteLine("Dossier manquant...");
                 Directory.CreateDirectory(FOLDER_PATH);
-                Trace.WriteLine("Dossier crée");
             }
 
             using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
             {
                 serializer.Serialize(fileStream, account);
             }
-
-            Trace.WriteLine("Fin de l'écriture");
         }
     }
 }
