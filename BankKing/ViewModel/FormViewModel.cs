229661ee1eaaafe1caae9bb28d51c0ba2150689d
Léo Puyastier <puyastier.leo@gmail.com>
2025-12-10 18:58:02 +0100

    View Modèle Basique (à détailer)

---
+++ BankKing/ViewModel/FormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BankKing.ViewModel
+{
+    public class FormViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
