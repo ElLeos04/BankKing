b8c1d288e758add552985c9df3767a20ce9d69b0
Léo Puyastier <puyastier.leo@gmail.com>
2025-12-16 14:08:48 +0100

    Création du VM

---
+++ BankKing/ViewModel/Form/AddTransactionViewModel.cs
@@ -0,0 +1,50 @@
+﻿using BankKing.Data.Entry;
+
+namespace BankKing.ViewModel.Form;
+
+public class AddTransactionViewModel : FormViewModel
+{
+    private EntryType _transactionType;
+    public EntryType TransactionType
+    {
+        get => _transactionType;
+        set
+        {
+            _transactionType = value;
+            OnPropertyChanged(nameof(TransactionType));
+        }
+    }
+
+    private EntryCategory? _category;
+    public EntryCategory? Category
+    {
+        get => _category;
+        set
+        {
+            _category = value;
+            OnPropertyChanged(nameof(Category));
+        }
+    }
+
+    private decimal _amount = 0m;
+    public decimal Amount
+    {
+        get => _amount;
+        set
+        {
+            _amount = value;
+            OnPropertyChanged(nameof(Amount));
+        }
+    }
+
+    private DateTime? _date;
+    public DateTime? Date
+    {
+        get => _date;
+        set
+        {
+            _date = value;
+            OnPropertyChanged(nameof(Date));
+        }
+    }
+}
