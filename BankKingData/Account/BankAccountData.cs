0237d5f9cb53584c2f40d24230d4f0d9c75ada0b
Léo Puyastier <puyastier.leo@gmail.com>
2026-01-05 20:40:16 +0100

    Création des comptes BO

--- BankKingData/Account/BankAccountData.cs
+++ BankKingData/Account/BankAccountData.cs
@@ -6,10 +6,6 @@ public class BankAccountData
 {
     public required string Name { get; set; }
     public decimal Balance { get; set; }
-    public List<AccountEntryData> Entries { get; set; }
+    public required List<AccountEntryData> Entries { get; set; }
 
-    public BankAccountData()
-    {
-        Entries = [];
-    }
 }
