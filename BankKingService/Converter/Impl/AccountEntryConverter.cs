094801b4cd86367a9c588ee146b7a861158be922
Léo Puyastier <puyastier.leo@gmail.com>
2026-01-06 15:36:15 +0100

    Converter pour entrée de compte

---
+++ BankKingService/Converter/Impl/AccountEntryConverter.cs
@@ -0,0 +1,45 @@
+﻿using BankKingData.Entry;
+using BankKingService.Data;
+
+namespace BankKingService.Converter.Impl;
+
+public class AccountEntryConverter : IAccountEntryConverter
+{
+    public List<AccountEntryData> BOListToDataList(List<AccountEntryBO> boList)
+    {
+        return boList.ConvertAll(BOToData);
+    }
+
+    public AccountEntryData BOToData(AccountEntryBO bo)
+    {
+        return new AccountEntryData
+        {
+            Category = new EntryCategoryData
+            {
+                Name = bo.Category.Name,
+                Type = bo.Category.Type
+            },
+            Amount = bo.Amount,
+            Date = bo.Date
+        };
+    }
+
+    public List<AccountEntryBO> DataListToBOList(List<AccountEntryData> dataList)
+    {
+        return dataList.ConvertAll(DataToBO);
+    }
+
+    public AccountEntryBO DataToBO(AccountEntryData data)
+    {
+        return new AccountEntryBO
+        {
+            Category = new EntryCategoryBO
+            {
+                Name = data.Category.Name,
+                Type = data.Category.Type
+            },
+            Amount = data.Amount,
+            Date = data.Date
+        };
+    }
+}
