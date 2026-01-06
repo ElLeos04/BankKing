d560c2f876a71e634fbf4e317cb84f9dcafe9c24
Léo Puyastier <puyastier.leo@gmail.com>
2026-01-06 15:20:04 +0100

    Converter pour categories

---
+++ BankKingService/Converter/Impl/EntryCategoryConverter.cs
@@ -0,0 +1,36 @@
+﻿
+using BankKingData.Entry;
+using BankKingService.Data;
+
+namespace BankKingService.Converter.Impl;
+
+public class EntryCategoryConverter : IEntryCategoryConverter
+{
+    public List<EntryCategoryData> BOListToDataList(List<EntryCategoryBO> boList)
+    {
+        return boList.ConvertAll(BOToData);
+    }
+
+    public EntryCategoryData BOToData(EntryCategoryBO bo)
+    {
+        return new EntryCategoryData
+        {
+            Name = bo.Name,
+            Type = bo.Type
+        };
+    }
+
+    public List<EntryCategoryBO> DataListToBOList(List<EntryCategoryData> dataList)
+    {
+        return dataList.ConvertAll(DataToBO);
+    }
+
+    public EntryCategoryBO DataToBO(EntryCategoryData data)
+    {
+        return new EntryCategoryBO
+        {
+            Name = data.Name,
+            Type = data.Type
+        };
+    }
+}
