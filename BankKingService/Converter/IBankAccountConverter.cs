829ee622a6a963507b91b7327ecf9a70bc23a440
Léo Puyastier <puyastier.leo@gmail.com>
2026-01-06 15:47:57 +0100

    Converter pour comptes bancaires

---
+++ BankKingService/Converter/IBankAccountConverter.cs
@@ -0,0 +1,15 @@
+﻿using BankKingData.Account;
+using BankKingService.Data;
+
+namespace BankKingService.Converter;
+
+public interface IBankAccountConverter
+{
+    BankAccountBO DataToBO(BankAccountData data);
+
+    BankAccountData BOToData(BankAccountBO bo);
+
+    List<BankAccountData> BOListToDataList(List<BankAccountBO> boList);
+
+    List<BankAccountBO> DataListToBOList(List<BankAccountData> dataList);
+}
---
+++ BankKingService/Converter/Impl/BankAccountConverter.cs
@@ -0,0 +1,37 @@
+﻿using BankKingData.Account;
+using BankKingService.Data;
+
+namespace BankKingService.Converter.Impl;
+
+public class BankAccountConverter(IAccountEntryConverter accountEntryConverter) : IBankAccountConverter
+{
+    public List<BankAccountData> BOListToDataList(List<BankAccountBO> boList)
+    {
+        return boList.ConvertAll(BOToData);
+    }
+
+    public BankAccountData BOToData(BankAccountBO bo)
+    {
+        return new BankAccountData
+        {
+            Name = bo.Name,
+            Balance = bo.Balance,
+            Entries = bo.Entries != null ? accountEntryConverter.BOListToDataList(bo.Entries) : []
+        };
+    }
+
+    public List<BankAccountBO> DataListToBOList(List<BankAccountData> dataList)
+    {
+        return dataList.ConvertAll(DataToBO);
+    }
+
+    public BankAccountBO DataToBO(BankAccountData data)
+    {
+        return new BankAccountBO
+        {
+            Name = data.Name,
+            Balance = data.Balance,
+            Entries = data.Entries != null ? accountEntryConverter.DataListToBOList(data.Entries) : []
+        };
+    }
+}
