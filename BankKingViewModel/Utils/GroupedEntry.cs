27dd8a2d620af5cea363c45787de6d37ff111ee7
Léo Puyastier <puyastier.leo@gmail.com>
2026-01-08 18:37:14 +0100

    VM Utils

---
+++ BankKingViewModel/Utils/GroupedEntry.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BankKingViewModel.Utils;
+
+public class GroupedEntry<T>(string key, IEnumerable<T> items) : ObservableCollection<T>(items)
+{
+    public string Key { get; set; } = key;
+}
--- BankKingViewModel/Utils/IDialogService.cs
+++ BankKingViewModel/Utils/IDialogService.cs
@@ -6,8 +6,13 @@
 /// <remarks>Implementations of this interface allow applications to present dialogs in a decoupled manner,
 /// typically for user interaction or confirmation. The dialog is shown modally, blocking interaction with other windows
 /// until it is closed.</remarks>
-/// <typeparam name="T">The type that represents the view aspect or content to be displayed in the dialog. Specific to each display app.</typeparam>
-public interface IDialogService<T>
+public interface IDialogService
 {
-    bool ShowDialog(T viewAspect);
+    /// <summary>
+    /// Displays a modal dialog for the specified view aspect and waits for the user to close it.
+    /// </summary>
+    /// <typeparam name="T">The type of the view aspect to display in the dialog.</typeparam>
+    /// <param name="viewAspect">The view aspect to be shown in the dialog. Cannot be null.</param>
+    /// <returns>true if the dialog was accepted by the user; otherwise, false.</returns>
+    bool ShowDialog<T>(T viewAspect);
 }
