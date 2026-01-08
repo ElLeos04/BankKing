48af6819d477fe488e4de060c4bd2864c5031199
Léo Puyastier <puyastier.leo@gmail.com>
2026-01-08 14:49:51 +0100

    Déplacement du VM pour Entry

---
+++ BankKingViewModel/HistoryEntryViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BankKingData.Entry;
+
+namespace BankKingViewModel
+{
+    public class HistoryEntryViewModel(AccountEntryData _accountEntry) : BaseViewModel
+    {
+
+        public DateTime Date => _accountEntry.Date;
+        public string DateText => _accountEntry.Date.ToShortDateString();
+
+
+        public string CategoryName => _accountEntry.Category.Name;
+
+        public string AmountText => _accountEntry.Amount.ToString("C2");
+
+        public bool IsPositive => _accountEntry.Amount >= 0;
+
+        // Mock constructor for design-time data
+        public HistoryEntryViewModel() : this(new AccountEntryData() { Category = new() { Name = "Category", Type = EntryType.None }, Amount = 12345, Date = DateTime.Today })
+        {
+        }
+    }
+}
