2bb71c560d5dc5dd1a3b77c08ca4ceabaec76f16
Léo Puyastier <puyastier.leo@gmail.com>
2026-01-08 13:53:19 +0100

    VM de bases

---
+++ BankKingViewModel/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace BankKingViewModel;
+
+public abstract class BaseViewModel : INotifyPropertyChanged
+{
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged(string name)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
