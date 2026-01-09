3be1612d853c4a741aa4e95b339a893376591855
Léo Puyastier <puyastier.leo@gmail.com>
2026-01-09 15:29:18 +0100

    Implémentation DialogService (à renommer ?) et modif package app

--- BankKingViewModel/Utils/IDialogService.cs
+++ BankKingViewModel/Utils/IDialogService.cs
@@ -1,7 +1,7 @@
 ﻿namespace BankKingViewModel.Utils;
 
 /// <summary>
-/// Defines a service for displaying a modal dialog using a specified view aspect.
+/// Defines a service for displaying a modal dialog. Used for dependency injection.
 /// </summary>
 /// <remarks>Implementations of this interface allow applications to present dialogs in a decoupled manner,
 /// typically for user interaction or confirmation. The dialog is shown modally, blocking interaction with other windows
@@ -11,8 +11,19 @@ public interface IDialogService
     /// <summary>
     /// Displays a modal dialog for the specified view aspect and waits for the user to close it.
     /// </summary>
-    /// <typeparam name="T">The type of the view aspect to display in the dialog.</typeparam>
-    /// <param name="viewAspect">The view aspect to be shown in the dialog. Cannot be null.</param>
+    /// <param name="viewAspect">The object view aspect linked</param>
     /// <returns>true if the dialog was accepted by the user; otherwise, false.</returns>
-    bool ShowDialog<T>(T viewAspect);
+    bool ShowDialog(object viewAspect);
+}
+
+
+
+/// <summary>
+/// Typed version of IDialogService for a specific view type. Should hold the concrete implementation of ShowDialog.
+/// </summary>
+/// <typeparam name="TView">Type of the view used by the platform</typeparam>
+public interface IDialogService<TView> : IDialogService
+{
+    // The specific method from your original question
+    bool ShowDialog(TView view);
 }
---
+++ BankKingWPF/Utils/WPFDialogService.cs
@@ -0,0 +1,35 @@
+﻿using BankKingViewModel.Form;
+using BankKingViewModel.Utils;
+using BankKingWPF.UI;
+using System.Windows;
+
+namespace BankKingWPF.Utils;
+
+public class WPFDialogService : IDialogService<FormViewModel>
+{
+    public bool ShowDialog(FormViewModel viewModel)
+    {
+        Window window = new FormWindow()
+        {
+            DataContext = viewModel
+        };
+
+        viewModel.CloseRequested += (result) =>
+        {
+            window.DialogResult = result;
+            window.Close();
+        };
+
+        return window.ShowDialog() ?? false;
+    }
+
+    public bool ShowDialog(object viewAspect)
+    {
+        if (viewAspect is FormViewModel formViewModel)
+        {
+            return ShowDialog(formViewModel);
+        }
+
+        throw new ArgumentException("Unsupported view aspect type", nameof(viewAspect));
+    }
+}
