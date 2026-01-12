856292386f52ef8ceab8665676497c5711e67f4c
Léo Puyastier <puyastier.leo@gmail.com>
2026-01-12 21:00:02 +0100

    Revert "Remplacement des services"
    This reverts commit c6d33b6431fa717369af28a6bd3fd4eea694239c.

--- BankKingWPF/App.xaml.cs
+++ BankKingWPF/App.xaml.cs
@@ -23,8 +23,8 @@ public partial class App : Application
         ServiceCollection services = new();
 
         // Register Services
-        services.AddKeyedSingleton<BankKingData.IDataIO, BankKingData.Impl.XMLAccountIO>("BankAccount");
-        services.AddKeyedSingleton<BankKingData.IDataIO, BankKingData.Impl.XMLCategoryIO>("Category");
+        services.AddSingleton<BankKingData.ICategoryIO, BankKingData.Impl.XMLCategoryIO>();
+        services.AddSingleton<BankKingData.IAccountIO, BankKingData.Impl.XMLAccountIO>();
 
         services.AddSingleton<BankKingService.IAccountService, BankKingService.Impl.AccountService>();
         services.AddSingleton<BankKingService.ICategoryService, BankKingService.Impl.CategoryService>();
