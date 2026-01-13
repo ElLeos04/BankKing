49b0dd1ab22f67857dc66dd378d0d91d61c24204
Léo Puyastier <puyastier.leo@gmail.com>
2026-01-13 14:58:39 +0100

    Design formulaire de confirmation

--- BankKingViewModel/Form/ConfirmationFormViewModel.cs
+++ BankKingViewModel/Form/ConfirmationFormViewModel.cs
@@ -2,4 +2,15 @@
 
 public class ConfirmationFormViewModel : FormViewModel
 {
+
+    private string _message = "";
+    public string Message
+    {
+        get => _message;
+        set
+        {
+            _message = value;
+            OnPropertyChanged(nameof(Message));
+        }
+    }
 }
