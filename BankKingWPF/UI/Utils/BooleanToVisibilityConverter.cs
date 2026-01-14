fb9d9d2cd28ec94109825ee8abfd78d4d40e9cb0
Léo Puyastier <puyastier.leo@gmail.com>
2026-01-14 15:39:15 +0100

    Ajout bouton suppressoin pour style + converter

---
+++ BankKingWPF/UI/Utils/BooleanToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace BankKingWPF.UI.Utils;
+
+class BooleanToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            return boolValue ? System.Windows.Visibility.Visible : System.Windows.Visibility.Collapsed;
+        }
+        return System.Windows.Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
